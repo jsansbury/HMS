8c5c5f793e6ee8896b53c6a23f73fad044005842
jsansbury <jane.sansbury@petroc.ac.uk>
2022-01-07 10:43:30 +0000

    Revert "tidied up"
    This reverts commit a05d17b680d9dd63d89b4adc17897b73a6fca10a.

---
+++ Appliance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HMS
+{
+    abstract class Appliance
+    {
+        public string color = "white";
+        public string location;
+        abstract public void move(string n);
+
+  /*      public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public string Location
+        {
+            get { return location; }
+            set { color = value; }
+        }*/
+
+    }
+}
